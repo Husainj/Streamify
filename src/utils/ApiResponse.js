65ae0a84e992a0829c5809abd980e82fca12b1d2
Husainj <jhalodwalahusain30@gmail.com>
2024-06-21 12:04:17 +0530

    Updated user , video model

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -3,6 +3,6 @@ class ApiResponse{
         this.statusCode = statusCode
         this.data = data
         this.message=message
-        this.statusCode = statusCode < 400
+        this.success = statusCode < 400
     }
-}
+} 
