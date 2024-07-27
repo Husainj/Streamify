96ce2aa015408f20fb0bd3c1bae12dc5ef86456b
Husainj <jhalodwalahusain30@gmail.com>
2024-07-27 19:19:30 +0530

    Playlist controller and routes added

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,12 +28,14 @@ import router from "./routes/user.routes.js"
 import videoRouter from "./routes/video.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
 
 //routes declaration
 app.use("/api/v1/users" , userRouter)
 app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/playlist", playlistRouter)
 
 
 export default router
--- backend/src/controllers/subscription.controller.js
+++ backend/src/controllers/subscription.controller.js
@@ -104,6 +104,7 @@ const getUserChannelSubscribers = asyncHandler(async (req, res) => {
 })
 
 // controller to return channel list to which user has subscribed
+//✅
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
 
