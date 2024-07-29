1d2dd6f333281b0f76a98256a6e79b891edd216d
Husainj <jhalodwalahusain30@gmail.com>
2024-07-29 18:46:53 +0530

    Comment file added

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,6 +30,7 @@ import tweetRouter from "./routes/tweet.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
 import likeRouter from "./routes/like.routes.js"
+import commentRouter from "./routes/comment.routes.js"
 
 //routes declaration
 app.use("/api/v1/users" , userRouter)
@@ -38,6 +39,7 @@ app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
 app.use("/api/v1/playlist", playlistRouter)
 app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/comments", commentRouter)
 
 export default router
 
