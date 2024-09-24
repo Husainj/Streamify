c63478ce16f4bf55d87c5014147a513a3694164f
Husainj <jhalodwalahusain30@gmail.com>
2024-09-24 22:02:20 +0530

    temp Directory created to solve register error

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -1,14 +1,20 @@
-import multer from "multer";
+import fs from 'fs';
+import path from 'path';
+import multer from 'multer';
+
+// Ensure the directory exists
+const tempDir = './public/temp';
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true }); // create the directory if it doesn't exist
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    cb(null, tempDir);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
   },
 });
 
-export const upload = multer({
-  storage,
-});
+export const upload = multer({ storage });
