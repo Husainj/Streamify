b22ba5138286970363d29a06612d160ee0c640e3
Husainj <jhalodwalahusain30@gmail.com>
2024-09-24 22:12:37 +0530

    multer code

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -1,20 +1,16 @@
-import fs from 'fs';
-import path from 'path';
-import multer from 'multer';
-
-// Ensure the directory exists
-const tempDir = './public/temp';
-if (!fs.existsSync(tempDir)) {
-  fs.mkdirSync(tempDir, { recursive: true }); // create the directory if it doesn't exist
-}
+import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, tempDir);
+    cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
   },
 });
 
-export const upload = multer({ storage });
+export const upload = multer({
+  storage,
+});
+
+
