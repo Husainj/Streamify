bfa922d5db52bdf0959c3c188c6fec4665bb6e77
Husainj <jhalodwalahusain30@gmail.com>
2024-09-25 21:45:00 +0530

    multer update 2

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -1,27 +1,16 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import multer from 'multer';
-
-// Reconstruct __dirname in ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Define absolute path for the public/temp directory
-const tempDir = path.join(__dirname, '..', 'public', 'temp');
-
-// Ensure the directory exists
-if (!fs.existsSync(tempDir)) {
-  fs.mkdirSync(tempDir, { recursive: true }); // create the directory if it doesn't exist
-}
+import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, tempDir);
+    cb(null, './public/temp');
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
   },
 });
 
-export const upload = multer({ storage });
+export const upload = multer({
+  storage,
+});
+
+
