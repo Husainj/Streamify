0e009afe909fe4ee3dd55bef083929b1307d9f7e
Husainj <jhalodwalahusain30@gmail.com>
2024-10-07 23:46:59 +0530

    app . options cors added

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ app.use(cors({
     origin : process.env.CORS_ORIGIN ,
     credentials: true
 }))
-
+app.options('*', cors());
 //Accepting json also with size limit
 app.use(express.json({limit:"50mb"}))
  
