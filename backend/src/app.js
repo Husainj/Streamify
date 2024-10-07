d4d0cc59853914090a536ac97e0f1d01cc52a1b4
Husainj <jhalodwalahusain30@gmail.com>
2024-10-07 23:36:46 +0530

    size of file increased

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,11 +11,11 @@ app.use(cors({
 }))
 
 //Accepting json also with size limit
-app.use(express.json({limit:"16kb"}))
+app.use(express.json({limit:"50mb"}))
  
 //  used to accept url's and encode them , like we need to tell express that space " " is converted into %20 ,
 //  similarly it will help in encoding it.
-app.use(express.urlencoded({extended:true , limit:"16kb"}))
+app.use(express.urlencoded({extended:true , limit:"50mb"}))
 
 //to keep assets or images , see you have made a public folder 
 app.use(express.static("public")) 
--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -3,4 +3,9 @@ import multer from 'multer';
 // Use memory storage to handle files in memory
 const storage = multer.memoryStorage();
 
-export const upload = multer({ storage });
+export const upload = multer({ 
+    storage,
+    limits: {
+        fileSize: 50 * 1024 * 1024 // Limit to 50 MB
+      }
+ });
